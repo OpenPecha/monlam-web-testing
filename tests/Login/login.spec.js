cb7353260aba5441ff169ae94856d1ea067d55a1
Karcee <karmat359@gmail.com>
2024-06-26 09:46:50 +0530

    text integration and code optimization

--- tests/Login/login.spec.js
+++ tests/Login/login.spec.js
@@ -1,35 +1,33 @@
-import { test,expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pageObject/LoginPage';
+require('dotenv').config();
 
-test("Is logged In", async({ page })=> {
-    const login = new LoginPage(page)
-    await login.goTo()
-    const isLoggedIn = await login.isLoggedIn()
-    console.log(isLoggedIn)
-})
-test("Valid login", async ({page}) => {
-    const email = "karmat359@gmail.com"
-    const password = "Kartse3181#"
-    const login = new LoginPage(page)
-    await login.goTo()
-    const isLoggedIn = await login.login(email, password)
-    expect(isLoggedIn).toBe(true)
-})
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+
+test("Valid login", async ({ page }) => {
+    const login = new LoginPage(page);
+    await login.goTo();
+    const isLoggedIn = await login.login(email, password);
+    expect(isLoggedIn).toBe(true);
+});
+
 test("Wrong email login", async ({ page }) => {
-    const email = "karmat@gmail.com"
-    const password = "Kartse3181#"
-    const login = new LoginPage(page)
-    await login.goTo()
-    const isLoggedIn = await login.login(email, password)
-    expect(isLoggedIn).toBe(false)
-
-})
+    const wrongEmail = "wrongemail@example.com";
+    const login = new LoginPage(page);
+    await login.goTo();
+    const isLoggedIn = await login.login(wrongEmail, password);
+    expect(isLoggedIn).toBe(false);
+    const errorMsg = await login.errorMsg.textContent()
+    expect(errorMsg).toBe('Wrong email or password.')
+});
 
 test("Wrong password login", async ({ page }) => {
-    const email = "karmat359@gmail.com"
-    const password = "Kartse3181"
-    const login = new LoginPage(page)
-    await login.goTo()
-    const isLoggedIn = await login.login(email, password)
-    expect(isLoggedIn).toBe(false)
-})
+    const wrongPassword = "wrongpassword";
+    const login = new LoginPage(page);
+    await login.goTo();
+    const isLoggedIn = await login.login(email, wrongPassword);
+    expect(isLoggedIn).toBe(false);
+    const errorMsg = await login.errorMsg.textContent()
+    expect(errorMsg).toBe('Wrong email or password.')
+});
